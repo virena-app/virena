3f6e05700d05937e9051c21600c7ad641b06c39e
Samuel Hickey <sam.august@gmail.com>
2018-09-18 18:58:57 -0400

    add generate template files

---
+++ src/utils/generateNavigatorTemplate.util.js
@@ -0,0 +1,53 @@
+/**
+ * Populates dropdown menu with all available parents to add a new child to
+ * @param {array} sortableTree - The current state of the sortable tree
+ */
+
+export const getAllParents = sortableTree => {
+  return sortableTree.reduce( (parentsArr, branch) => {
+    return branch.type !== "Screen" ? parentsArr.concat(branch.title, getAllParents(branch.children)) : parentsArr
+  }, []);
+}
+
+/**
+ * Flattens the sortable tree / redux store state
+ * @param {array} sortableTree - The current state of the sortable tree
+ */
+
+export const flattenTree = sortableTree => {
+  return sortableTree.reduce((flattenedTree, comp) => {
+    return comp.children ? flattenedTree.concat({...comp, children: comp.children.map(child => child.id)}, flattenTree(comp.children)) : flattenedTree.concat(comp)
+  }, [])
+}
+
+/**
+ * Required to generate the navigator.js file
+ * @param {array} components - The flattened version of the sortable tree state
+ * @param {object} parent - A parent node whose immediate children's titles we want
+ */
+
+export const getImmediateChildrenTitles = (components, parent) => {
+  return components.filter(screen => {
+    for (let i = 0; i < parent.children.length; i++) {
+      if (screen.id === parent.children[i]) return true;
+    }
+    return false;
+  }).map(child => child.title.replace(/\s+/g, ''));
+}
+
+/**
+ * Master function which generates navigator.js file
+ * @param {array} screens - The flattened version of the sortable tree state
+ */
+  
+export const generateNavigatorFile = screens => {
+  const unspace = title => title.replace(/\s+/g, '');
+  const navigators = screens.filter( screen => screen.children );
+  return navigators.map(navigator => {
+    const childrenTitles = getImmediateChildrenTitles(screens, navigator);
+    return `const ${unspace(navigator.title)} = create${navigator.type}Navigator({` + (childrenTitles.length ?
+    childrenTitles.map(title => {
+      return `${title}: { screen: ${title} }`
+    }).join(', ') + '})' : '})')
+  }).join('\n\n') + `\n\nexport default ${unspace(screens[0].title)}`;
+}
