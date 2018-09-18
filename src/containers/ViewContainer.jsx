ce8b2e707a312f9350457501c3c2cf33f48ac267
gerretkubota <gtkubota@cpp.edu>
2018-09-18 16:46:51 -0700

    Testing to see if the images will populate based upon the selection of the component

--- src/containers/ViewContainer.jsx
+++ src/containers/ViewContainer.jsx
@@ -8,7 +8,40 @@ const mapStateToProps = store => ({
 const mapDispatchToProps = dispatch => ({})
 export default class ViewContainer extends Component {
   render() {
-    const image = `../assets/${this.props.selectedComponent}`;
+    const image = '../assets/';
+    const selected = this.props.selectedComponent;
+    
+    if(selected) {
+      switch(selected.type) {
+        case 'BottomTab':
+          if(selected.children.length === 1) image += '1tab.png';
+          if(selected.children.length === 2) image += '2tab.png';
+          if(selected.children.length === 3) image += '3tab.png';
+          if(selected.children.length === 4) image += '4tab.png';
+          if(selected.children.length === 5) image += '5tab.png';
+          break;
+        case 'Drawer':
+        if(selected.children.length === 1) image += '1drawer.png';
+        if(selected.children.length === 2) image += '2drawer.png';
+        if(selected.children.length === 3) image += '3drawer.png';
+        if(selected.children.length === 4) image += '4drawer.png';
+        if(selected.children.length === 5) image += '5drawer.png';
+        break;
+          break;
+        case 'Simple Screen':
+          break;
+        case 'Stack':
+          break;
+        case 'Switch':
+          break;
+        default:
+          image += 'defauilt.png'
+      }
+
+    }else {
+      image += 'default.png';
+    }
+
     return (
       <div className='view'>
         {/* <img id='iphone' src='../assets/iphonex.png'></img> */}
