de71992a3ec1abf84ad83a84d365eeaf83479165
JChan <jchan@JCs-MacBook-Pro.local>
2018-09-24 22:55:45 -0700

    added bottom tab phone simulator

--- src/components/BottomTab.jsx
+++ src/components/BottomTab.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default class BottomTab extends Component {
   render() {
-    // const links = this.props.selectedComponent.children.map((screen, i) => <Link to={`/${screen.title}`} key={i}>{screen.title}</Link>)
+    const links = this.props.selectedComponent.children.map((screen, i) => <Link to={`/${screen.title}`} key={i}>{screen.title}</Link>)
     return (
       <div className='bottom-tab'>
-          {/* {links} */}
-          <Link to='/one' replace>One</Link>
-          <Link to ='/two' replace>Two</Link>
+          {links}
+          {/* <Link to='/one' replace>One</Link>
+          <Link to ='/two' replace>Two</Link> */}
       </div>
     )
   }
--- src/components/PhoneScreen.jsx
+++ src/components/PhoneScreen.jsx
@@ -6,13 +6,15 @@ import Three from './Three.jsx';
 
 export default class PhoneScreen extends Component {
   render() {
-    // const routes = () => this.props.selectedComponent.children? this.props.selectedComponent.children.map((screen, i) => <Route path={`/${screen.title}`} render={() => <div>{screen.title}</div>} />): <Route exact path='/' render={() => <div>Default</div>} />;
+    const routes = () => {
+      if (this.props.selectedComponent.children) return this.props.selectedComponent.children.map((screen, i) => <Route path={`/${screen.title}`} render={() => <div>{screen.title}</div>} />)
+    }
     return (
       <div className='phone-screen'>
         <Switch>
-          {/* {routes()} */}
-          <Route exact path='/One' render={() => <div>One</div>} />
-          <Route path='/Two' render={() => <div>Two</div>} />
+          {routes()}
+          {/* <Route exact path='/One' render={() => <div>One</div>} />
+          <Route path='/Two' render={() => <div>Two</div>} /> */}
         </Switch>
       </div>
     )
