81cd9cbb30d68d99014cbd74a7ddb7b46dd280f1
JChan <jchan@JCs-MacBook-Pro.local>
2018-09-25 19:16:49 -0700

    added nested structure dynamic phone screen

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 export default class Drawer extends Component {
   render() {
     let drawerClass = 'drawer';
+    const getNodeKey = ({ treeIndex }) => treeIndex;
     if (this.props.drawerState) {
       drawerClass= 'drawer open'
     }
 
-    const links = () => {if (this.props.selectedComponent.children) return this.props.selectedComponent.children.map((screen, i) => <Link to={`/${screen.title}`}>{screen.title}</Link>)}
+    const links = () => {if (this.props.selectedComponent.children) return this.props.selectedComponent.children.map((screen, i) => {
+      if (screen.subtitle === 'Simple Screen') {
+        return <Link to={`/${screen.title}`}>{screen.title}</Link>
+      }
+      else if (screen.subtitle === 'BottomTab') return <button onClick={() => this.props.selectComponent(screen.title, screen.subtitle, screen.children, getNodeKey, screen.path)}>{screen.title}</button>
+    })}
 
     return (
       <div className={drawerClass}>
--- src/containers/ViewContainer.jsx
+++ src/containers/ViewContainer.jsx
@@ -1,23 +1,10 @@
 import React, {Component} from 'react';
-// import * as actions from '../actions/actions';
-import { populateImg } from '../utils/populateImg';
-import { connect } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import PhoneContainer from './PhoneContainer.jsx';
 
-const mapStateToProps = store => ({
-  selectedComponent: store.data.selectedComponent,
-  treeData: store.data.treeData,
-})
-
-const mapDispatchToProps = dispatch => ({})
 class ViewContainer extends Component {
 
   render() {
-    // let image = '../assets/';
-    // const selected = this.props.selectedComponent;
-    // image = populateImg(image, selected, this.props.treeData);
-    // console.log(`image here ${image}`);
     return (
       <div className='view'>
         <div className='inner-view'>
@@ -25,10 +12,9 @@ class ViewContainer extends Component {
               <PhoneContainer />
             </HashRouter>
         </div>
-        {/* <img id='iphone' src={image}></img> */}
       </div>
     )
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ViewContainer);
+export default ViewContainer;
