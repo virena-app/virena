9824908d4480ee180b83d44736d9571529d991ce
gerretkubota <gerretkubota@gmail.com>
2018-09-29 13:36:05 -0700

    All of the phone screens should be working. Fixed the error where if you delete any component the app crashes

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 export default class Switch extends Component {
   render() {
+    const switchClass = 'switch-button'
     return (
-      <div className='switch-button'>
+      <div className={switchClass}>
         <button onClick={() => this.props.selectComponent(this.props.child)}>{this.props.selectedComponent.title}</button>
       </div>
     )
