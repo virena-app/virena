8d4777b5053461e8eba8608e5f62c5d027e99e30
JChan <jcisbroke@gmail.com>
2018-10-01 12:37:52 -0700

    added basic oauth

---
+++ login.js
@@ -0,0 +1,103 @@
+const {parse} = require('url')
+const {remote, ipcRenderer} = require('electron')
+const qs = require('qs')
+const axios = require('axios')
+
+const GOOGLE_AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth'
+const GOOGLE_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token'
+const GOOGLE_PROFILE_URL = 'https://www.googleapis.com/userinfo/v2/me'
+
+async function googleSignIn () {
+  const code = await signInWithPopup()
+  const tokens = await fetchAccessTokens(code)
+  console.log(tokens)
+  const {id, email, name} = await fetchGoogleProfile(tokens.access_token)
+  const providerUser = {
+    uid: id,
+    email,
+    displayName: name,
+    idToken: tokens.id_token,
+  }
+
+  return ipcRenderer.send('authorized', providerUser)
+}
+
+function signInWithPopup () {
+  return new Promise((resolve, reject) => {
+    const authWindow = new remote.BrowserWindow({
+      width: 500,
+      height: 600,
+      show: true,
+    })
+
+    // TODO: Generate and validate PKCE code_challenge value
+    const urlParams = {
+      response_type: 'code',
+      redirect_uri: 'http://127.0.0.1:8000',
+      client_id: '484818166811-l38imlkp6f9s2t4p0c8mt9vui0vf7f0q.apps.googleusercontent.com',
+      scope: 'profile email',
+    }
+    const authUrl = `${GOOGLE_AUTHORIZATION_URL}?${qs.stringify(urlParams)}`
+
+    function handleNavigation (url) {
+      const query = parse(url, true).query
+      console.log(query)
+      if (query) {
+        if (query.error) {
+          reject(new Error(`There was an error: ${query.error}`))
+        } else if (query.code) {
+          authWindow.removeAllListeners('closed')
+          setImmediate(() => authWindow.close())
+
+          resolve(query.code)
+        }
+      }
+    }
+
+    authWindow.on('closed', () => {
+      // TODO: Handle this smoothly
+      throw new Error('Auth window was closed by user')
+    })
+
+    authWindow.webContents.on('will-navigate', (event, url) => {
+      handleNavigation(url)
+    })
+
+    authWindow.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
+      handleNavigation(newUrl)
+    })
+
+    authWindow.loadURL(authUrl)
+  })
+}
+
+async function fetchAccessTokens (code) {
+  console.log('code')
+  const response = await axios.post(GOOGLE_TOKEN_URL, qs.stringify({
+    code,
+    client_id: '484818166811-l38imlkp6f9s2t4p0c8mt9vui0vf7f0q.apps.googleusercontent.com',
+    redirect_uri: 'http://127.0.0.1:8000',
+    grant_type: 'authorization_code',
+  }), {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+  return response.data
+}
+
+async function fetchGoogleProfile (accessToken) {
+  console.log('fetch')
+  const response = await axios.get(GOOGLE_PROFILE_URL, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+    },
+  })
+  return response.data
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+  googleSignIn()
+})
--- main.js
+++ main.js
@@ -1,25 +1,46 @@
 const electron = require('electron')
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 const url = require('url');
 const path = require('path');
+// const express = require('express');
+// const expressApp = express();
+
+// expressApp.listen(3000, () => console.log('Listening on 3000'))
 
 let win;
 
 const createWindow = () => {
 
   const {width, height} = electron.screen.getPrimaryDisplay().size;
-
   win = new BrowserWindow({width, height});
 
   if (process.env.NODE_ENV === 'development') win.loadURL('http://localhost:8080')
   else win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.resolve(__dirname, 'login.html'),
     protocol: 'file:',
-    hash: '/',
     slashes: true
   }));
 
+  // if (process.env.NODE_ENV === 'development') win.loadURL('http://localhost:8080')
+  // else win.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file:',
+  //   hash: '/',
+  //   slashes: true
+  // }));
+
   win.webContents.openDevTools() 
+
+  ipcMain.on('authorized', (event, args) => {
+    if (args) {
+      win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        hash: '/',
+        slashes: true
+      }));
+    }
+  })
 
   win.on('closed', () => {
     win = null
