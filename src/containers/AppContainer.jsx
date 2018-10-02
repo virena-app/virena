a022171638ffe7e8c2324f88df7ca3b790c6d9bb
JChan <jcisbroke@gmail.com>
2018-10-02 10:26:29 -0700

    conditional rendering based on login status

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,19 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import TopNav from '../components/TopNav.jsx';
 import ViewContainer from './ViewContainer.jsx';
 import TreeContainer from './TreeContainer.jsx';
 import PanelContainer from './PanelContainer.jsx';
 
+const mapStateToProps = store => ({
+  userLoggedIn: store.data.userLoggedIn
+})
 
-export default class AppContainer extends Component {
+const mapDispatchToProps = dispatch => ({
+
+})
+
+class AppContainer extends Component {
   componentDidMount() {
     console.log('AppContainer Mounted!')
   }
   render() {
+    const { userLoggedIn } = this.props
     console.log('AppContainer Rendered')
     return (
       <div>
-        <TopNav />
+        <TopNav userLoggedIn={userLoggedIn}/>
         <div className='main'>
         <ViewContainer />
         <div className='vertical-line'></div>
@@ -25,3 +34,5 @@ export default class AppContainer extends Component {
     )
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps) (AppContainer)
