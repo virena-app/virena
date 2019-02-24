1ab53a0184387c979e8dc5b0f5aa1e2b4e97b1d1
Jacky Chan <jcisbroke@gmail.com>
2019-02-25 01:12:20 +0800

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-export const db = new Sequelize('postgres://master:password@visualize-react-native.cdl88obocnjd.us-east-2.rds.amazonaws.com:5432/visualize_react_native', {dialect: 'postgres'});
+export const db = new Sequelize(/*database here*/);
 
 export const Project = db.define('projects', {
   treeData: Sequelize.JSONB,
